ba1c6f3cba326270641e3dfbba8105a23a6a6627
Claudio7768 <locarno.claudio@gmail.com>
2026-03-06 19:59:11 +0100

    Add files via upload

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,4 +4,3 @@ const SUPABASE_URL = 'https://sfivaxkutmgonhwjfqtx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmaXZheGt1dG1nb25od2pmcXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MDU2OTAsImV4cCI6MjA4ODM4MTY5MH0.G2DcZk4zKb8qvtStR8KHKhreeeBExL5T-Ma2DXeZvjo';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
